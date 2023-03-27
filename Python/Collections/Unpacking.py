7b7b8993f516fda72a0fa0b14f8dee036818be56
Timothy Enderson <timothyenderson01@gmail.com>
2023-03-27 08:53:12 +0200

    Added Unpacking

---
+++ Python/Collections/Unpacking.py
@@ -0,0 +1,25 @@
+# Unpacking Is A Way Of Assigning Values To Variables From A Sequence Or Collection
+
+# These Are A Few Types I Know Of That Can Be Unpacked: Strings, Lists, Tuples, Dictionaries And Sets
+
+aList = ["blue", "aqua", "teal", "lightblue", "night blue"]
+
+aTuple = ("banana", "apple", "orange", "grape", "strawberry")
+
+# Now To Unpack The List And Tuple Into Variables You Can Write The Following:
+
+(element1, element2, element3, element4, element5) = aList
+
+print(element1)
+print(element2)
+print(element3)
+print(element4)
+print(element5)
+
+(fruit1, fruit2, fruit3, fruit4, fruit5) = aTuple
+
+print(fruit1)
+print(fruit2)
+print(fruit3)
+print(fruit4)
+print(fruit5)
